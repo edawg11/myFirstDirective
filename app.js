ded3964eef3ff25f7ed660d34072bbb4acea4e27
edawg11 <eliot.smith1185@gmail.com>
2016-03-21 11:01:59 -0700

    added tab selecting

--- app.js
+++ app.js
@@ -5,9 +5,45 @@ angular.module('app', [])
   return {
     restrict: 'E',
     transclude: true,
-    template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
+    require: '^tabset',
+    template: '<div role="tabpanel" ng-show="active" ng-transclude></div>',
+    scope: { 
+      heading: '@',
+    },
+    link: function(scope, elem, attr, tabsetCtrl) {
+      scope.active = false;
+      tabsetCtrl.addTab(scope);
+    }
+  }
+})
+.directive('tabset', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
     scope: { },
-    link: function(scope, elem, attr) {}
+    templateUrl: 'tabset.html',
+    bindToController: true,
+    controllerAs: 'tabset',
+    controller: function(){
+      var self = this;
+      self.tabs = [];
+
+      self.select = function(selectedTab) {
+        angular.forEach(self.tabs, function(tab) {
+          if(tab.active && tab !== selectedTab){
+            tab.active = false;
+          };
+        })
+        selectedTab.active = true;
+      };
+      self.addTab = function addTab(tab) {
+        self.tabs.push(tab);
+
+        if(self.tabs.length === 1){
+          tab.active = true;
+        }
+      };
+    }
   }
 })
 // Define directives here
